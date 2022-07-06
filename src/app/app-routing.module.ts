89fc61425e754d471c5c12160b405a8327c7700c
Daniel Luna <danieluna91@gmail.com>
2022-07-06 01:52:54 -0500

    estructura con json finalizada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
 {path: 'products', component: ProductsComponent},
 { path: 'store-locator', loadChildren: () => import('./pages/store-locator/store-locator.module').then(m => m.StoreLocatorModule) },
 { path: '', loadChildren: () => import('./pages/index/index.module').then(m => m.IndexModule) },
+{ path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) },
 {path: '**', redirectTo:'',pathMatch:'full'}
 
 ];
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import ProductsJson from '../../data.json';
+import * as data from '../../data.json';
 
 
 interface PRODUCTS {
@@ -12,6 +13,22 @@ interface PRODUCTS {
   stock: Number;
 }
 
+interface CATEGORIES {
+  id: Number;
+  nameCategory: String;
+}
+
+interface AB {
+  id: Number;
+  name: String;
+  price: Number;
+  description: String;
+  categoryId: Number;
+  stock: Number;
+  nameCategory: String;
+}
+
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -19,14 +36,37 @@ interface PRODUCTS {
 })
 export class ProductsComponent implements OnInit {
 
+/*   private LOGO = require("../../shared/images/1.jpg"); */
 
+  products2: any = (data as any).default;
   ngOnInit(): void {
   }
 
-  Products: PRODUCTS[] = ProductsJson.products;
+  products: any = (ProductsJson.productosTemporal as any).default;
+
+/*   Products: PRODUCTS[] = ProductsJson.products;
+
+  Categories: CATEGORIES[] = ProductsJson.categories;
  
+  Categories: CATEGORIES[] = ProductsJson.categories;
+ */
+
   constructor(){
-    console.log(JSON.stringify(this.Products));
+    console.log(JSON.stringify(this.products));
+    console.log(JSON.stringify(this.products2.productosTemporal));
   }
+
+
+ /*  const mergeAB = this.Products.map( PRODUCTS => {
+    const matched = this.Products.find(CATEGORIES => CATEGORIES.id === PRODUCTS.categoryId)
+    if(matched) {
+      return {...this.Products, ...matched}
+    } else {
+      // return companyA element or customize it with your case
+    }
+  }
+) */
+
+
 
 }
